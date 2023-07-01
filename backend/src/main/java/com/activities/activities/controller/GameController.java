25e4c34239c1e6d4445d245d8ca51ca458e8a71b
andrejtodorovski <82031894+andrejtodorovski@users.noreply.github.com>
2023-07-02 01:14:03 +0200

    Bug fixes

--- backend/src/main/java/com/activities/activities/controller/GameController.java
+++ backend/src/main/java/com/activities/activities/controller/GameController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/games")
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin(origins={"http://localhost:4200","http://activities.201084.com'})
 public class GameController {
     private final GameService gameService;
 
